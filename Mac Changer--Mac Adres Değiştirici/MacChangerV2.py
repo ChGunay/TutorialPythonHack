49b3435f02867d2b6aaedf7b93004a8930fcd816
ChGunay <chgunay3@gmail.com>
2021-03-31 21:26:05 +0300

    Update MacChangerV2.py

--- Mac Changer--Mac Adres Değiştirici/MacChangerV2.py
+++ Mac Changer--Mac Adres Değiştirici/MacChangerV2.py
@@ -4,14 +4,21 @@ import optparse#We add the optparse library to the script so that the content of
 
 parse_object = optparse.OptionParser()#We create an object using the optparse library and using this object we can get options from the user.--Optparse kütüphanesini kullanarak bir nesne oluşturuyoruz ve bu nesneyi kullanarak kullanıcıdan seçenekler alabiliyoruz.
 parse_object.add_option("-i","--interface", dest="interface", help = "interface to change!")#We direct the options we receive from the user to the variables in the script with dest.--Kullanıcıdan aldığımız seçenekleri dest ile betikteki değişkenlere yönlendiriyoruz.
+parse_object.add_option("-m","--mac",dest="mac_adress", help= "New mac address")#We direct the options we receive from the user to the variables in the script with dest.--Kullanıcıdan aldığımız seçenekleri dest ile betikteki değişkenlere yönlendiriyoruz.
 
+#print(parse_object.parse_args())#show change which we did--yaptığımız değişiklikleri gösterir.
 
-interface = "wlan0"
-mac_adress= "10:00:22:44:55:44"
+(user_options,arguments) = parse_object.parse_args()
+print(user_options.interface)#show our selected inteface--seçilen interface gösterir
+
+print(user_options.mac_adress)#show our new mac address--yeni mac adresimizi gösterir
+
+interface = user_options.interface
+mac_adress= user_options.mac_adress
 
 print("Mac Changer Started ")
 subprocess.call(["ifconfig",interface,"down"])#First of all, we disable our internet tool.--Öncelikle internet aracımızı devre dışı bırakıyoruz.
-subprocess.call(["ifconfig",interface,"hw","ether","00:11:22:33:44:55"]) #By stating that we will make changes in the hardware, we enter our new mac address.--Donanımda değişiklik yapacağımızı belirterek yeni mac adresimize giriyoruz.  
+subprocess.call(["ifconfig",interface,"hw","ether",mac_adress]) #By stating that we will make changes in the hardware, we enter our new mac address.--Donanımda değişiklik yapacağımızı belirterek yeni mac adresimize giriyoruz.  
 subprocess.call(["ifconfig",interface,"down"])#Finally, we activate our internet tool.--Son olarak internet aracımızı etkinleştiriyoruz.
 animation = "|/-\\"
 idx = 0
